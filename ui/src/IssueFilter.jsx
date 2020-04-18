4b83c51d1315910d49cbfb7b4ccb1a978d230876
pengwenyu <1031781561@qq.com>
2020-04-18 15:58:26 -0700

    first commit

---
+++ ui/src/IssueFilter.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+/* eslint "react/prefer-stateless-function": "off" */
+
+// eslint-disable-next-line react/prefer-stateless-function
+export default class IssueFilter extends React.Component {
+  render() {
+    return (
+      <div>Showing all avalible products</div>
+    );
+  }
+}
